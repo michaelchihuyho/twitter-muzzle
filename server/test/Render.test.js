c517de96eb34d18d50087b125c48255d6b7ec822
Michael Ho <michaelchihuyho@gmail.com>
2017-02-12 22:01:59 -0800

    Add render test

---
+++ server/test/Render.test.js
@@ -0,0 +1,31 @@
+var should = require('should')
+  , rewire = require('rewire')
+  , sinon = require('sinon')
+
+require('should-sinon')
+
+var Render = rewire('../lib/module/render/Render')
+
+describe('#renderApp()', function() {
+    it('render index.pug with the write app name', function() {
+        var req = {}
+          , res = {}
+          , tweets = []
+
+        Render.__set__({
+            clientStats: {
+                publicPath: 'http://some.path/'
+              , assetsByChunkName: {
+                    main: 'appname.js'
+                }
+            }
+        })
+        res.render = sinon.stub()
+
+        Render.renderApp(req, res)
+
+        res.render.should.be.calledWith('index.pug', {
+            clientAppUrl: 'http://some.path/appname.js.gzip'
+        })
+    })
+})
